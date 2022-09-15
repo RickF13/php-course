7b53ab8272c6d34ebee99705bcc182a01c052cf1
RickII R <f13.rick@gmail.com>
2022-09-15 18:42:00 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+        <?php
+            echo "Hello World PHP! <br/>";
+            echo "Welcome, I'm a PHP script!";
+        ?>
+
+    </body>
+</html>
